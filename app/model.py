54a4d36f2a73b07da843bcfb9ae4f1daf4940f6a
sheila <arijoane@gmail.com>
2018-06-24 02:14:56 +0300

    changed api  routes

--- app/model.py
+++ app/model.py
@@ -1,5 +1,5 @@
 """a class representing the user model"""
-class Passengers:
+class Passenger:
     def __init__(self,name,username,email,contact,country,password,confirm_password):
         self.name = name
         self.username = username
@@ -9,7 +9,7 @@ class Passengers:
         self.password = password
         self.confirm_password = confirm_password
 
-class driver:
+class Driver:
     """A class representing the Driver Model"""
     def __init__(self, name,username,email,contact,country,password,confirm_password,licence_number,number_plate):
         self.name = name
@@ -22,7 +22,7 @@ class driver:
         self.licence_number = licence_number
         self.number_plate = number_plate
 
-class Ride_Offer:
+class RideOffer:
     """A class representing the Ride_offermodel"""
     def __init__(self,driver,cartype,number_plate,destination,fare,mobile,pick):
         self.driver = driver 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+from app.model import RideOffer, Passenger, Driver
 from flask import request, Flask, json, jsonify
 
 rides = { }
@@ -7,39 +8,16 @@ rides = { }
 def get_rides():
     pass
 
-@app.route("/api/v1/rides/get>",methods=['GET'])
+@app.route("/api/v1/rides/<int:ride_id>",methods=['GET'])
 def get_single_ride():
     pass
 
 
-@app.route("/api/v1/rides/create", methods=['POST'])
+@app.route("/api/v1/rides", methods=['POST'])
 def create_ride():
     pass
 
-@app.route("/api/v1/rides/join", methods=['POST'])
+@app.route("/api/v1/rides/<int:ride_id>/requests", methods=['POST'])
 def join_ride():
     pass
 
-"""
-@app.route("/api/v1/createride", methods=['GET','POST'])
-def create_ride():
-    # creates a ride_offer
-    if 
-
-    
-
-@app.route("/rides", methods=['GET'])
-def ride_offers():
-    # fetches all ride offers 
-    if request.method == 'GET':
-        return ride, 200
-
-
-
-@app.route("/rideId>",method=['GET'])
-def single_ride_offer():
-    # fetches a single ride_offer from the ride_-offer list
-    if request.method == 'GET':
-        ride_offers  = ride_offers.query.get(id=id)
-        return{"message":"succeeded" },200
-"""
