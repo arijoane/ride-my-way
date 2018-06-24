61382b227bdc8ec911007d0ac76bd9641c5a5409
sheila <arijoane@gmail.com>
2018-06-24 16:13:23 +0300

    added tests

--- tests.py
+++ tests.py
@@ -1,6 +1,18 @@
 import unittest
-
+from app.model  import RideOffer, Passenger, Driver
 class TestRidemyway(unittest.TestCase):
 
     def test_it_creates_ride(self):
         pass
+
+    def test_it_creates_driver(self):
+        pass
+
+    def test_it_returns_rides(self):
+        pass
+
+    def test_it_finds_ride(self):
+        pass
+
+    def test_it_returns_drivers(self):
+        pass      
